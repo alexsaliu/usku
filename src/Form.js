bf58104163649742b9ab6941efc336e8b8442600
BaysheffDev <42050015+BaysheffDev@users.noreply.github.com>
2020-04-16 11:22:36 +0930

    Removed postcode fields and made cost field show conditionally

--- src/Form.js
+++ src/Form.js
@@ -10,14 +10,12 @@ const Form = () => {
     // const [response, setResponse] = useState("");
     const [name, setName] = useState("");
     const [deliveryAddress, setDeliveryAddress] = useState("");
-    const [postcode, setPostcode] = useState("");
     const [phone, setPhone] = useState("");
     const [item, setItem] = useState("");
     const [prePurchase, setPrePurchase] = useState("");
     const [cost, setCost] = useState("");
     const [pickupName, setPickupName] = useState("");
     const [pickupAddress, setPickupAddress] = useState("");
-    const [pickupPostcode, setPickupPostcode] = useState("");
     const [details, setDetails] = useState("");
 
     const [prePurchaseSelected, setPrePurchaseSelected] = useState("");
@@ -36,9 +34,8 @@ const Form = () => {
     const sendOrder = () => {
         setSubmitted(true);
         console.log("Pre purchase selected");
-        if (!name || !deliveryAddress || !postcode || !phone
-            || !item || (!cost && !prePurchase) || !prePurchaseSelected || !pickupName
-            || !pickupAddress || !pickupPostcode || !details)
+        if (!name || !deliveryAddress || !phone || !item || (!cost && !prePurchase)
+            || !prePurchaseSelected || !pickupName || !pickupAddress || !details)
         {
             console.log("missing field");
             return;
@@ -64,8 +61,6 @@ const Form = () => {
             ${name}\n
             *Delivery Address:*
             ${deliveryAddress}\n
-            *Postcode:*
-            ${postcode}\n
             *Phone:*
             ${phone}\n
             *Item(s):*
@@ -78,8 +73,6 @@ const Form = () => {
             ${pickupName}\n
             *Pickup Address:*
             ${pickupAddress}\n
-            *Pickup Postcode:*
-            ${pickupPostcode}\n
             *Delivery Instructions:*
             ${details}\n
         `
@@ -103,10 +96,6 @@ const Form = () => {
                     <input onChange={(e) => setDeliveryAddress(e.target.value)} type="text" value={deliveryAddress} />
                 </div>
                 <div className="input-container">
-                    <label className={(!postcode && submitted) ? 'red' : ''}>Postcode</label>
-                    <input onChange={(e) => setPostcode(e.target.value)} type="text" value={postcode} />
-                </div>
-                <div className="input-container">
                     <label className={(!phone && submitted) ? 'red' : ''}>Phone</label>
                     <input onChange={(e) => setPhone(e.target.value)} type="text" value={phone} />
                 </div>
@@ -114,13 +103,13 @@ const Form = () => {
                     <label className={(!item && submitted) ? 'red' : ''}>What would you like delivered?</label>
                     <input onChange={(e) => setItem(e.target.value)} type="text" value={item} />
                 </div>
-                <div className="input-container">
+                <div style={{margin: '10px 0'}} className="input-container">
                     <label className={(!prePurchaseSelected && submitted) ? 'red' : ''}>Have you pre ordered and paid for your product?</label>
                     <input onClick={(e) => {setPrePurchase(true); setPrePurchaseSelected(true)}} type="radio" name="prePurchase" /> Yes
                     <input onClick={(e) => {setPrePurchase(false); setPrePurchaseSelected(true)}} type="radio" name="prePurchase" /> No
                 </div>
-                <div className="input-container">
-                    <label className={(!cost && !prePurchase && submitted) ? 'red' : ''}>If no, What is the estimated price of product?</label>
+                <div style={!prePurchase ? {display: 'none'} : {display: 'block'}} className="input-container">
+                    <label className={(!cost && !prePurchase && submitted) ? 'red' : ''}>What is the estimated price of product?</label>
                     <input onChange={(e) => setCost(e.target.value)} type="text" value={cost} />
                 </div>
                 <div className="input-container">
@@ -130,10 +119,6 @@ const Form = () => {
                 <div className="input-container">
                     <label className={(!pickupAddress && submitted) ? 'red' : ''}>Pickup Address</label>
                     <input onChange={(e) => setPickupAddress(e.target.value)} type="text" value={pickupAddress} />
-                </div>
-                <div className="input-container">
-                    <label className={(!pickupPostcode && submitted) ? 'red' : ''}>Pickup Postcode</label>
-                    <input onChange={(e) => setPickupPostcode(e.target.value)} type="text" value={pickupPostcode} />
                 </div>
                 <div className="input-container">
                     <label className={(!details && submitted) ? 'red' : ''}>Delivery Instructions</label>
